e6c3c3de635ef058a5bb96ab81d43d930805df1b
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-04-15 18:00:29 +0200

    xxxxxx

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,8 @@ module.exports = {
   singleQuote: true,
   printWidth: 120,
   tabWidth: 2,
+<<<<<<< HEAD
+=======
+  endOfLine: 'auto',
+>>>>>>> parent of 0c0ed26 (update obisidian package and allow no parens in arrow funcs)
 };
