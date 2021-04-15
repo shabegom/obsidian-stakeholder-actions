f328489d3cedd658f9c32015d4e42668085c0647
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-04-15 17:59:24 +0200

    rolling back

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,16 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 3223152 (Merge branch 'main' into typing-cleanup)
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
+<<<<<<< HEAD
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
@@ -60,3 +64,14 @@ module.exports = {
 =======
 };
 >>>>>>> parent of 55bb7fd (Merge pull request #21 from shabegom/typing-cleanup)
+=======
+  plugins: ['prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Should be last
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
+>>>>>>> parent of 3223152 (Merge branch 'main' into typing-cleanup)
